382511057e31e2470a2fd733fe7cf6c7ded2645f
Kreidler, Matyas Edgar <88744380+MatyiHI@users.noreply.github.com>
2023-05-09 10:53:46 -1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
 scene.setBackgroundImage(assets.image`Background`)
+let bunny = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(bunny)
+bunny.setStayInScreen(true)
